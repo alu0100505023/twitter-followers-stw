baaeeb3b0ea63d7397edd04049746398ec50b60f
Javier C Rodriguez <alu0100505023@ull.edu.es>
2014-09-25 12:13:41 +0100

    Creando lista de amigos

--- twitter.rb
+++ twitter.rb
@@ -4,6 +4,8 @@ require 'sinatra'
 
 require './configure'
 
+set :port, 9449
+
 get '/' do
   @todo_tweet = []
   @name = ''
@@ -18,9 +20,26 @@ post '/' do
   @number = params[:n].to_i
   @number = 1 if @number <= 0
   if client.user? @name 
-    ultimos_t = client.user_timeline(@name,{:count=>@number.to_i})
-    @todo_tweet =(@todo_tweet != '') ? ultimos_t.map{ |i| i.text} : ''
+	
+  #  ultimos_t = client.user_timeline(@name,{:count=>@number.to_i})
+   # @todo_tweet =(@todo_tweet != '') ? ultimos_t.map{ |i| i.text} : ''	
+
+	#ultimos_t =  client.friend_ids(@name).attrs[:ids].take(@number)
+	#amigos={}
+   	
+	#ultimos_t.map { amigos[client.user.name] = client.user.followers_count }
+	#(f)
+ 	#@todo_tweet=amigos.sort_by { |x, y| -y }
+	ultimos_t=client.friends(@name,{})
+	
+	ultimos_t=ultimos_t.map { |i| [i.name , i.followers_count]}
+
+	@todo_tweet=ultimos_t.sort_by{[:followers_count]}.reverse!.take(@number)
+	
+		
+
   end
+
   erb :twitter
 end
 
